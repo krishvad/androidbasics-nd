448f4e09bf75a3e5a3cff426be7b715df7a12133
LEELA SRIVATSAVA VADDE <Krishna@Krishna-mp.local>
2016-12-16 17:52:38 -0800

    changed activity names

--- app/src/main/java/com/example/android/udacitymusicapp/FamousPop.java
+++ app/src/main/java/com/example/android/udacitymusicapp/FamousPop.java
@@ -1,18 +1,7 @@
 package com.example.android.udacitymusicapp;
 
-import android.content.ActivityNotFoundException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 /**
  * Created by leela_vadde on 12/11/16.
@@ -22,7 +11,7 @@ public class FamousPop extends AppCompatActivity{
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
-        setContentView(R.layout.audio_books);
+        setContentView(R.layout.pop_culture);
 
 
     }
